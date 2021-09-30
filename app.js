3bada8324188310e86dea6d2569387981413ff0e
aarkas <90221994+aarkas@users.noreply.github.com>
2021-09-30 18:42:07 -0500

    Update 2.0

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const choices = ['paper', 'scissors', 'rock']
 
+const buttons = document.querySelectorAll('.btn')
+
+console.log(pickRandom());
+
+function pickRandom() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
