03ffb2cf6baa8a0596689e25c6d85d6546919271
aarkas <90221994+aarkas@users.noreply.github.com>
2021-09-30 19:55:25 -0500

    Update 3.0

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-const choices = ['paper', 'scissors', 'rock']
+const choices = ['paper', 'rock', 'scissors'];
+const scoreElement = document.getElementById('score_points');
 
-const buttons = document.querySelectorAll('.btn')
+const buttons = document.querySelectorAll('.btn');
 
-console.log(pickRandom());
+let score = 0; 
+let userChoice = undefined;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        userChoice = button.getAttribute('data-choice');
+
+        Winner();
+    });
+});
+
+function Winner() {
+    const computerChoice = pickRandom();
+
+    if (userChoice === computerChoice) {
+
+    }   else if (
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'scissors' && computerChoice === 'paper') 
+            
+     ) {
+         updateScore(1);
+
+     } else {
+         updateScore(-1);
+
+     }
+
+}
+
+function updateScore(value) {
+    score += value;
+
+    scoreElement.innerText = score;
+}
 
 function pickRandom() {
-    return choices[Math.floor(Math.random() * choices.length)];
+    return choices[Math.floor(Math.random() * choices.length)]
+    ;
 }
